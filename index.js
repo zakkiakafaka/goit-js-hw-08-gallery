945e6b1fab7e37b5b5f86c188a35ff0d9a4ddf48
Zakhar Skyba <68905247+zakkiakafaka@users.noreply.github.com>
2020-12-03 23:10:16 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,72 @@
+import pictures from "./gallery-items.js";
+
+const gallery = document.querySelector('.js-gallery');
+const modal = document.querySelector('.js-lightbox');
+const modalBtn = document.querySelector('.lightbox__button');
+const modalImg = document.querySelector('.lightbox__image');
+const overlay = document.querySelector('.lightbox__overlay');
+let currentIndex = 0;
+
+pictures.forEach (el => {
+    gallery.insertAdjacentHTML('beforeend', 
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${el.original}">
+    <img class="gallery__image" src="${el.preview}" data-source="${el.original}" alt="${el.description}"/>
+    </a>
+    </li>
+    `)
+});
+
+const openModal = function (event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') return;
+    modal.classList.add('is-open');
+    modalImg.src = event.target.dataset.source;
+    console.log(modalImg.src);
+};
+
+const closeModal = function () {
+    modal.classList.remove('is-open')
+    modalImg.src = ""
+};
+
+const previousImg = () => {
+    if (currentIndex > 0 && currentIndex <= pictures.length - 1) {
+        currentIndex -= 1;
+        modalImg.src = pictures[currentIndex].original;
+    } else if (currentIndex === 0) {
+        currentIndex = pictures.length -1;
+        modalImg.src = pictures[currentIndex].original;
+    }
+};
+
+const nextImg = () => {
+    if (currentIndex >= 0 && currentIndex < pictures.length - 1) {
+      currentIndex += 1;
+      modalImg.src = pictures[currentIndex].original;
+    } else if ((currentIndex = pictures.length - 1)) {
+      currentIndex = 0;
+      modalImg.src = pictures[currentIndex].original;
+    }
+  };
+
+const keyClose = (e) => {
+    switch (e.key) {
+        case "Escape":
+            closeModal();
+            break;
+        case "ArrowLeft":
+            previousImg();
+            break;
+        case "ArrowRight":
+            nextImg();
+            break;
+        default:
+            return;
+    }
+};
+
+gallery.addEventListener('click', openModal);
+modalBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+window.addEventListener('keydown', keyClose);
